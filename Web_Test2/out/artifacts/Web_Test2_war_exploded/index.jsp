edd92eb6adfce53597c71ab2dee8091e6bf3ddc5
Minho Ji <59356172+minho4418@users.noreply.github.com>
2022-02-17 14:28:45 +0900

    Add files via upload

---
+++ Web_Test2/out/artifacts/Web_Test2_war_exploded/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wlals
+  Date: 2022-02-17
+  Time: 오후 12:11
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <meta charset="EUC-KR">
+  <title>ㅇㅇ</title>
+</head>
+<body>
+<form action="./userJoinAction.jsp" method="post">
+  <input type="text" name="userID">
+  <input type="password" name="userPassword">
+  <input type="submit" value="전송">
+</form>
+
+</body>
+</html>
+
---
+++ Web_Test2/src/util/DatabaseUtil.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil {
+    public static Connection getConnection() {
+        //데이터베이스와 연결상태 관리
+        try {
+            String dbURL = "jdbc:mysql://localhost:3306/testDB?useSSL=false&serverTimezone=UTC";
+            String dbID = "root";
+            String dbPassword = "";
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(dbURL, dbID, dbPassword);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
